04ac2f092cf413e2c61420740b219ef68ca23019
Gustavo Domingues <61640594+bigasdev@users.noreply.github.com>
2024-10-14 09:20:29 -0300

    Starting to add the zoom, not working on entity

--- src/entity/AssetScreen.cpp
+++ src/entity/AssetScreen.cpp
@@ -24,13 +24,13 @@ void AssetScreen::root() {
     for (int j = 50; j < g_engine->get_window_size()->y - 40;
          j += (m_grid_size.y * m_zoom)) {
       if (x == 1) {
-        g_renderer->draw_rect({i, j, static_cast<int>(m_grid_size.x),
-                               static_cast<int>(m_grid_size.y)},
+        g_renderer->draw_rect({i, j, static_cast<int>(m_grid_size.x*m_zoom),
+                               static_cast<int>(m_grid_size.y*m_zoom)},
                               {82, 250, 161, 55}, true);
         x = 0;
       } else {
-        g_renderer->draw_rect({i, j, static_cast<int>(m_grid_size.x),
-                               static_cast<int>(m_grid_size.y)},
+        g_renderer->draw_rect({i, j, static_cast<int>(m_grid_size.x*m_zoom),
+                               static_cast<int>(m_grid_size.y*m_zoom)},
                               {35, 205, 113, 65}, true);
         x++;
       }
@@ -38,7 +38,7 @@ void AssetScreen::root() {
   }
   g_renderer->draw_from_sheet(
       *g_res->get_texture(g_selected_entity->pallete_name), {0, 0},
-      {g_selected_entity->atlas_pos.x, g_selected_entity->atlas_pos.y,
+      {g_selected_entity->sprite_pos.x/(g_selected_entity->sprite_size.x*m_zoom), g_selected_entity->sprite_pos.y/(g_selected_entity->sprite_size.y*m_zoom),
        g_selected_entity->sprite_size.x*m_zoom, g_selected_entity->sprite_size.y*m_zoom});
 }
 
