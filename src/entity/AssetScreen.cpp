7431d345c2b9886a7866ca017bd2adb626657a58
Gustavo Domingues <61640594+bigasdev@users.noreply.github.com>
2024-10-14 08:04:55 -0300

    Found it, weird if from copilot

--- src/entity/AssetScreen.cpp
+++ src/entity/AssetScreen.cpp
@@ -20,24 +20,22 @@ void AssetScreen::root() {
          i += (16 * m_zoom)) {
       for (int j = 50; j < g_engine->get_window_size()->y - 40;
            j += (16 * m_zoom)) {
-        if (g_selected_entity->name == "asset") {
-          if (i % 2 == 0 || j % 2 == 0) {
-            g_renderer->draw_rect({i, j, 16, 16}, {82, 250, 161, 155}, true);
-          } else {
-            g_renderer->draw_rect({i, j, 16, 16}, {35, 205, 113, 165}, true);
-          }
+        if (i % 2 == 0 || j % 2 == 0) {
+          g_renderer->draw_rect({i, j, 16, 16}, {82, 250, 161, 55}, true);
+        } else {
+          g_renderer->draw_rect({i, j, 16, 16}, {35, 205, 113, 65}, true);
         }
       }
     }
   }
-  for(int i = 320; i < g_engine->get_window_size()->x -40; i+= (16*m_zoom)){
+  for (int i = 320; i < g_engine->get_window_size()->x - 40;
+       i += (16 * m_zoom)) {
     g_renderer->draw_rect({i, 50, 16, 16}, {255, 0, 0, 255}, true);
   }
-  g_renderer->draw_text({20, 20}, "Test", g_res->get_font("arial"), {255, 255, 255, 255}, 1, 200);
+  g_renderer->draw_text({20, 20}, "Test", g_res->get_font("arial"),
+                        {255, 255, 255, 255}, 1, 200);
 }
 
-void AssetScreen::ent() {
-}
+void AssetScreen::ent() {}
 
-void AssetScreen::ui() {
-}
+void AssetScreen::ui() {}
