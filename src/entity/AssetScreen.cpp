5a77771c4883f03deecd00c1b5a55219215549b6
Gustavo Domingues <61640594+bigasdev@users.noreply.github.com>
2024-10-15 09:34:45 -0300

    Asset grid working

--- src/entity/AssetScreen.cpp
+++ src/entity/AssetScreen.cpp
@@ -17,9 +17,9 @@ void AssetScreen::update() {
 
 void AssetScreen::root() {
   int x = 0;
-  for (int i = 380; i < g_engine->get_window_size()->x - 40;
+  for (int i = 384; i < g_engine->get_window_size()->x - 32;
        i += (m_grid_size.x * m_zoom)) {
-    for (int j = 50; j < g_engine->get_window_size()->y - 40;
+    for (int j = 48; j < g_engine->get_window_size()->y - 48;
          j += (m_grid_size.y * m_zoom)) {
       if (x == 1) {
         g_renderer->draw_rect({i, j, static_cast<int>(m_grid_size.x * m_zoom),
@@ -38,7 +38,7 @@ void AssetScreen::root() {
     return;
 
   g_renderer->draw_from_sheet(
-      *g_res->get_texture(g_selected_entity->pallete_name), {(400)-(g_selected_entity->sprite_size.x*m_zoom), 0},
+      *g_res->get_texture(g_selected_entity->pallete_name), {(static_cast<float>(734))-(g_selected_entity->sprite_size.x*m_zoom), (static_cast<float>(321))-(g_selected_entity->sprite_size.y*m_zoom)},
       {g_selected_entity->atlas_pos.x, g_selected_entity->atlas_pos.y,
        g_selected_entity->sprite_size.x, g_selected_entity->sprite_size.y},
       m_zoom);
